ba5818040b37d2d9033dcddf62a03f2726d1b943
Arghya Guha <96729154+Arghya003@users.noreply.github.com>
2023-03-06 00:20:26 +0530

    Add files via upload

---
+++ server.js
@@ -0,0 +1,18 @@
+const express = require('express')
+const app= express()
+
+
+
+const connectDB=require('./config/db')
+connectDB();
+
+
+
+
+
+
+
+const PORT=process.env.PORT||3000;
+app.listen(PORT,()=>{
+    console.log(`listening to ${PORT}`)
+})
